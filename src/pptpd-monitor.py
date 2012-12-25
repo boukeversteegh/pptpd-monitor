dd07d277bd2d41944cf06ff6443346232d1c8e3d
Bouke Versteegh <boukeversteegh@gmail.com>
2012-12-25 22:23:05 +0100

    Clean up of code, added commentary. Hopes this helps :-)

--- src/pptpd-monitor.py
+++ src/pptpd-monitor.py
@@ -4,27 +4,28 @@ import re
 from datetime import datetime
 sessions = {}
 
+# Some regular expressions that match log entries
+# pptpd		pppd[<PID>]
+# ipup		<TIMESTAMP> ... pppd[PID]: ... ip-up <INTERFACE> <USERNAME> <IP4>
+# close		Sent <TX> bytes, received <RX> bytes
+# ppp_remoteip4	remote IP address <IP4>
+# ppp_localip4	local IP address <IP4>
 r_pptpd		= re.compile(r"pppd\[(\d+)\]")
 r_ipup		= re.compile(r"(.+?) [a-zA-Z0-9]+ pppd\[\d+\]: pptpd-logwtmp.so ip-up ([a-z0-9]+) ([a-z]+) (\d+\.\d+\.\d+\.\d+)")
 r_close		= re.compile(r"Sent (\d+) bytes, received (\d+) bytes")
 r_ppp_remoteip4	= re.compile(r"remote IP address (\d+\.\d+\.\d+\.\d+)")
 r_ppp_localip4	= re.compile(r"local IP address (\d+\.\d+\.\d+\.\d+)")
 
-fmt_timestamp = "%b %d %H:%M:%S"
-now = datetime.now().replace(microsecond=0)
+logfile = "/var/log/messages"    # pptpd will log messages in here if debug is enabled (/etc/ppp/pptpd-options)
+fmt_timestamp = "%b %d %H:%M:%S" # Timestamp format as it appears in the logfile.
+now = datetime.now().replace(microsecond=0) # Current time, don't need microsecond accuracy.
 
-def extractData(dictionary, regex, keys, string):
-  match = regex.search(string)
-  if match:
-    for i in range(len(keys)):
-      key = keys[i]
-      dictionary[key] = match.group(i+1)
-
-for line in file("/var/log/messages"):
+# Gather all session data from log
+for line in file(logfile):
   line = line.strip()
-  m =  r_pptpd.search(line)
-  if m:
-    userid = m.group(1)
+  match =  r_pptpd.search(line)
+  if match:
+    userid = match.group(1)
 
     sessions.setdefault(userid, {
       "interface":	None,
@@ -39,29 +40,39 @@ for line in file("/var/log/messages"):
       "timestamp_open":	None,
     })
     session = sessions[userid]
-    extractData(session, r_ppp_remoteip4, ['ppp_remoteip4'], line)
+
+    # Read remoteip4 from line and store in session
+    match = r_ppp_remoteip4.search(line)
+    if match:
+      session['ppp_remoteip4'] = match.group(1)
+    
+    # PPTP session started
     m_ipup  = r_ipup.search(line)
-    m_close = r_close.search(line)
     if m_ipup:
       timestamp = m_ipup.group(1)
       interface	= m_ipup.group(2)
       username	= m_ipup.group(3)
       ip4	= m_ipup.group(4)
+      session['status']         = 'open'
       session['timestamp_open']	= datetime.strptime(timestamp, fmt_timestamp).replace(year=datetime.now().year)
       session['interface']	= interface
       session['username']	= username
       session['ip4']		= ip4
-      session['status']		= 'open'
+    
+    # PPTP session closed
+    m_close = r_close.search(line)
     if m_close:
       tx = int(m_close.group(1))
       rx = int(m_close.group(2))
-      session['tx']    += tx
-      session['rx']    += rx
-      session['total'] += tx + rx
       session['status'] = 'closed'
-    #print "User connected: ", m.group(1)
+      session['tx']     += tx
+      session['rx']     += rx
+      session['total']  += tx + rx
 
 import os
+
+# Gets TX-RX for a network interface, for example ppp0
+# This is used to get statistics on active sessions
 def getInterfaceTotals(interface):
   result = os.popen("ifconfig " + interface, "r")
   r_ipconfig = re.compile(r"RX bytes:(\d+) .+  TX bytes:(\d+)")
@@ -70,11 +81,12 @@ def getInterfaceTotals(interface):
     if m_ipconfig:
       return (int(m_ipconfig.group(2)), int(m_ipconfig.group(1)))
     
-
-# User Totals
+# Gather statistics per user
 users = {}
 for sessionid, session in sessions.iteritems():
-  users.setdefault(session['username'], {
+  username = session['username']
+  # Get userdata or set defaults
+  user = users.setdefault(username, {
     "tx":             0,
     "rx":             0,
     "ctx":            0,
@@ -89,38 +101,41 @@ for sessionid, session in sessions.iteritems():
     "interface":      None,
     "timestamp_open": None
   })
-  user = users[session['username']]
   
   # Current Session Open
   if session['status'] == 'open':
     user['session']       = session
     user['interface']     = session['interface']
     user['ip4']           = session['ip4']
-    user['ppp_remoteip4']	= session['ppp_remoteip4']
+    user['ppp_remoteip4'] = session['ppp_remoteip4']
     
     ctx, crx = getInterfaceTotals(session['interface'])
     user['crx'] = crx
     user['ctx'] = ctx
-    user['timestamp_open'] = session['timestamp_open']  
-  # Old Session
+    user['timestamp_open'] = session['timestamp_open']
+  
+  # Old Session (Put IP addresses in parens)
   elif user['ppp_remoteip4'] is None:
     user['ppp_remoteip4'] = '(' + str(session['ppp_remoteip4']) + ')'
-    user['ip4'] = '(' + str(session['ip4']) + ')'
+    user['ip4']           = '(' + str(session['ip4']) + ')'
+  
   # Totals
-  user['lastseen'] = session['timestamp_open']
-  user['tx'] += session['tx']
-  user['rx'] += session['rx']
+  user['lastseen'] =  session['timestamp_open'] # Will be overwritten by each session until the last.
+  user['tx']       += session['tx']
+  user['rx']       += session['rx']
   user['sessions'] += 1
-  user['total'] += session['tx'] + session['rx']
+  user['total']    += session['tx'] + session['rx']
+  
   if session['status'] == "open":
     user['sessions_open'] += 1
 
+# Convert bytes to human readable format
 def sizeof_fmt(num):
-    for x in ['b','KB','MB','GB']:
+    for x in ['b','KB','MB','GB','TB','PB','EB','ZB']:
         if num < 1024.0:
             return "%3.1f%s" % (num, x)
         num /= 1024.0
-    return "%3.1f%s" % (num, 'TB')
+    return "%3.1f%s" % (num, 'YB')
 
 
 print "PPTPD Client Statistics"
@@ -144,7 +159,6 @@ for username in users:
     print "  ",
   print str(username).ljust(15),
   print (str(user['sessions_open']) + "/" + str(user['sessions'])).rjust(6),
-  #print str(user['sessions_open']).rjust(4),
   print sizeof_fmt(user['rx']).rjust(8),
   print sizeof_fmt(user['tx']).rjust(8),
   print str(user['ip4']).rjust(18),
@@ -154,8 +168,6 @@ for username in users:
   print sizeof_fmt(user['crx']).rjust(8),
   try:
     print str(now - user['timestamp_open']).rjust(20),
-  #  print str(datetime.datetime.strptime(fmt_datetime, str(user['timestamp_open']))).rjust(20),
   except:
     print str(user['lastseen']).rjust(20),
   print ""
-#print stats
